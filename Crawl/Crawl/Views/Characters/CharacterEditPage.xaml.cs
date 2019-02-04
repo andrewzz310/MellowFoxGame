93c1aa3d8be1b1bea788d2c13647835af56dd858
Harshal Vadnere <vadnereh@seattleu.edu>
2019-02-03 22:39:48 -0800

    Code clean up.

--- Crawl/Crawl/Views/Characters/CharacterEditPage.xaml.cs
+++ Crawl/Crawl/Views/Characters/CharacterEditPage.xaml.cs
@@ -1,5 +1,4 @@
-﻿
-using System;
+﻿using System;
 using Crawl.Models;
 using Crawl.Controllers;
 using Crawl.ViewModels;
@@ -64,74 +63,10 @@ namespace Crawl.Views
             await Navigation.PopAsync();
         }
 
-        /*
-        // The stepper function for Range
-        void Age_OnStepperValueChanged(object sender, ValueChangedEventArgs e)
-        {
-            AgeValue.Text = String.Format("{0}", e.NewValue);
-        }
-        */
-
         // The stepper function for Value
         void LevelValue_OnStepperValueChanged(object sender, ValueChangedEventArgs e)
         {
             LevelValue.Text = String.Format("{0}", e.NewValue);
-        }
-
-        
+        }        
     }
 }
-
-
-/*
-using System;
-
-using Crawl.Models;
-using Crawl.ViewModels;
-using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
-
-namespace Crawl.Views
-{
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class CharacterEditPage : ContentPage
-	{
-	    // ReSharper disable once NotAccessedField.Local
-	    private CharacterDetailViewModel _viewModel;
-
-        public Character Data { get; set; }
-
-        public CharacterEditPage(CharacterDetailViewModel viewModel)
-        {
-            // Save off the item
-            Data = viewModel.Data;
-            viewModel.Title = "Edit " + viewModel.Title;
-
-            InitializeComponent();
-            
-
-            // Set the data binding for the page
-            BindingContext = _viewModel = viewModel;
-        }
-
-	    public async void Save_Clicked(object sender, EventArgs e)
-        {
-            MessagingCenter.Send(this, "EditData", Data);
-
-            // removing the old ItemDetails page, 2 up counting this page
-            Navigation.RemovePage(Navigation.NavigationStack[Navigation.NavigationStack.Count - 2]);
-
-            // Add a new items details page, with the new Item data on it
-            await Navigation.PushAsync(new CharacterDetailPage(new CharacterDetailViewModel(Data)));
-
-            // Last, remove this page
-            Navigation.RemovePage(this);
-        }
-
-	    private async void Cancel_Clicked(object sender, EventArgs e)
-        {
-            await Navigation.PopAsync();
-        }
-    }
-}
-*/
