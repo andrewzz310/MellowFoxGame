4e41b845989109ed1c957c090545eea2f2163181
maherhosn <38202267+maherhosn@users.noreply.github.com>
2019-02-17 22:14:47 -0800

    Added a Scores button to the home page

---
+++ Crawl/Crawl/Models/NewCharacter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Crawl.Models
+{
+    public class NewCharacter
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int Level { get; set; }
+
+        public int ExperienceTotal { get; set; }
+        public int Attack { get; set; }
+        public int Defense { get; set; }
+        public int Speed { get; set; }
+        public string ImageURI { get; set; }
+
+        //Preferred Item cause 2x damage
+        public PreferredItemEnum Item { get; set; }
+
+
+        public void Update(Character newData)
+        {
+            if (newData == null)
+            {
+                return;
+            }
+
+            // Update all the fields in the Data, except for the Id
+            Name = newData.Name;
+            Description = newData.Description;
+            Level = newData.Level;
+        }
+
+    }
+}
+ 
