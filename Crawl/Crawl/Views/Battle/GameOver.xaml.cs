58fa8f87f50f661556b7aa2d2ea781a54bab38ee
Andrew Zhu <andrewzz310@gmail.com>
2019-02-18 22:58:51 -0800

    adding gameover stats linked to scoredetails

--- Crawl/Crawl/Views/Battle/GameOver.xaml.cs
+++ Crawl/Crawl/Views/Battle/GameOver.xaml.cs
@@ -7,15 +7,34 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+using Crawl.Models;
+using Crawl.ViewModels;
+
+
 namespace Crawl.Views.Battle
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class GameOver : ContentPage
 	{
-		public GameOver ()
+        // scoredetailviewmodel to display scores at end of game
+        private ScoreDetailViewModel _viewModel;
+
+		public GameOver (ScoreDetailViewModel viewModel)
 		{
-			InitializeComponent ();
-		}
+            InitializeComponent();
+
+            BindingContext = _viewModel = viewModel;
+        }
+
+        public GameOver()
+        {
+            InitializeComponent();
+
+            var data = new Score();
+
+            _viewModel = new ScoreDetailViewModel(data);
+            BindingContext = _viewModel;
+        }
 
 
 
