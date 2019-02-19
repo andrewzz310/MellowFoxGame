5bf8b3fe01d5127aefaab90dedd3446bf5605089
Andrew Zhu <andrewzz310@gmail.com>
2019-02-18 21:00:23 -0800

    added more detail to battle round over page

--- Crawl/Crawl/Views/Battle/RoundOver.xaml.cs
+++ Crawl/Crawl/Views/Battle/RoundOver.xaml.cs
@@ -4,17 +4,85 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Crawl.Models;
+using Crawl.ViewModels;
+using Crawl.Views;
 
 namespace Crawl.Views.Battle
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class RoundOver : ContentPage
-	{
-		public RoundOver ()
-		{
-			InitializeComponent ();
-		}
-	}
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RoundOver : ContentPage
+    {
+        //viewmodel of battle with character and monster
+        private BattleViewModel _instance;
+
+        public RoundOver()
+        {
+
+            InitializeComponent();
+            BindingContext = _instance = BattleViewModel.Instance;
+
+        }
+        private async void OnCharacterSelected(object sender, SelectedItemChangedEventArgs args)
+        {
+            var data = args.SelectedItem as Character;
+            if (data == null)
+                return;
+
+            //do something for click like attack or something
+            await Navigation.PushAsync(new CharacterDetailPage(new CharacterDetailViewModel(data)));
+
+            // Manually deselect item.
+            CharactersBattle.SelectedItem = null;
+        }
+
+        private async void OnMonsterSelected(object sender, SelectedItemChangedEventArgs args)
+        {
+            var data = args.SelectedItem as Monster;
+            if (data == null)
+                return;
+
+            //do something for click like attack or something
+            await Navigation.PushAsync(new MonsterDetailPage(new MonsterDetailViewModel(data)));
+
+            //Manually deselect item.
+            MonstersBattle.SelectedItem = null;
+        }
+
+
+        //RoundOver
+        private async void NextRound_Command(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new BattleBeginPage());
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            BindingContext = null;
+
+            if (ToolbarItems.Count > 0)
+            {
+                ToolbarItems.RemoveAt(0);
+            }
+
+            InitializeComponent();
+
+            if (_instance.Dataset.Count == 0)
+            {
+                _instance.LoadDataCommand.Execute(null);
+            }
+            else if (_instance.NeedsRefresh())
+            {
+                _instance.LoadDataCommand.Execute(null);
+            }
+
+            BindingContext = _instance;
+        }
+    }
 }
