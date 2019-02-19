8f26788113de8fe6ae92b49d748e59fb9acae57e
Andrew Zhu <andrewzz310@gmail.com>
2019-02-18 23:39:53 -0800

    synched round over with items tray

--- Crawl/Crawl/Views/Battle/RoundOver.xaml.cs
+++ Crawl/Crawl/Views/Battle/RoundOver.xaml.cs
@@ -66,6 +66,7 @@ namespace Crawl.Views.Battle
             await Navigation.PushAsync(new GameOver());
         }
 
+        bool hasAppearedOnce = false;
         protected override void OnAppearing()
         {
             base.OnAppearing();
@@ -79,7 +80,8 @@ namespace Crawl.Views.Battle
 
             InitializeComponent();
 
-            if (_instance.DatasetChars.Count == 0)
+            if (_instance.DatasetChars.Count == 0 || _instance.DatasetMons.Count == 0 ||
+                _instance.DatasetItems.Count == 0)
             {
                 _instance.LoadDataCommand.Execute(null);
             }
